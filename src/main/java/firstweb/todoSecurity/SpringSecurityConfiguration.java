e7bde429bf414aca874faf6a44aa84fe9edd16cf
surendra <surendra1234@gmail.com>
2024-12-14 03:29:04 +0530

    Apply the spring securety with logout header and handlethe login with spring security

--- src/main/java/firstweb/todoSecurity/SpringSecurityConfiguration.java
+++ src/main/java/firstweb/todoSecurity/SpringSecurityConfiguration.java
@@ -6,33 +6,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SpringSecurityConfiguration {
 
-//	@Bean
-//	public InMemoryUserDetailsManager createUserDetailsManager() {
-//		UserDetails userDetails1 = createNewUser("admin","password");
-//		UserDetails userDetails2 = createNewUser("adminadmin", "pass");
-//		
-//		return new InMemoryUserDetailsManager(userDetails1,userDetails2);
-//
-//	}
-//
-//	private UserDetails createNewUser(String username, String password) {
-//		Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
-//		UserDetails userDetails = User.builder().passwordEncoder(passwordEncoder).username("admin").password("password")
-//				.roles("User", "Admin").build();
-//		return userDetails;
-//	}
-//
-//	@Bean
-//	public PasswordEncoder passwordEncoder() {
-//
-//		return new BCryptPasswordEncoder();
-//	}
-
+	@Bean
+	public InMemoryUserDetailsManager createUserDetailsManager() {
+		UserDetails userDetails = User.withDefaultPasswordEncoder()
+				.username("surendra")
+				.password("surend123" )
+				.roles("USER","ADMIN")
+				.build();
+		
+		return new InMemoryUserDetailsManager(userDetails);
+		 
+	}
 }
