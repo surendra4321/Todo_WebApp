bfe90cec905f4d8e8815edafac003b9f741eb989
surendra <surendra1234@gmail.com>
2024-12-17 23:31:23 +0530

    Configuration in spring security to get the  h2 console

--- src/main/java/firstweb/todoSecurity/SpringSecurityConfiguration.java
+++ src/main/java/firstweb/todoSecurity/SpringSecurityConfiguration.java
@@ -1,14 +1,18 @@
 package firstweb.todoSecurity;
 
 import java.util.function.Function;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+ 
 
 @Configuration
 public class SpringSecurityConfiguration {
@@ -36,4 +40,17 @@ public class SpringSecurityConfiguration {
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+		http.formLogin(Customizer.withDefaults());
+		http.csrf().disable();
+		http.headers().frameOptions().disable();
+		return http.build();
+	}
+
+	 
+
+	 
 }
